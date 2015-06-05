2e1554e2aadb7b93d2b765d4b92105ec3bef3d76
Jonathan Pepe <j.l.p.95@hotmail.com>
2015-06-04 21:55:08 -0300

    Mapa con los arreglos

--- src/mapa/Mapa.java
+++ src/mapa/Mapa.java
@@ -14,26 +14,21 @@ import src.mapa.*;
 public class Mapa {
 
 	private Map<Posicion,Mapeable> mapa;
+	private int tamanio;
 	
-	private static Mapa INSTANCE = null;
-	
-	private synchronized static void createInstance() {
-		if (INSTANCE == null) { 
-	       INSTANCE = new Mapa();
-	    }
-	}
-
-	public static Mapa getInstance() {
-	    if (INSTANCE == null) 
-	    	createInstance();
-	    return INSTANCE;
-	}
-	
-	public Mapa() {
+	public int getTamanioMapa(){
 		
+		return this.tamanio;
+	}
+	
+	public Mapa(int unTamanio) {
+		
+		this.tamanio = unTamanio;
 		this.mapa = new HashMap<Posicion,Mapeable>();
-		for (int i = 1; i <= ConstantesAlgoCraft.TAMANIO_MAPA; i++) {
-			for (int j = 1; j <= ConstantesAlgoCraft.TAMANIO_MAPA; j++) {
+		
+		for (int i = 1; i <= tamanio; i++) {
+			for (int j = 1; j <= tamanio; j++) {
+				
 				Posicion posicion = new Posicion(i,j);
 				Pasto pasto = new Pasto();
 				mapa.put(posicion, pasto);
@@ -41,57 +36,62 @@ public class Mapa {
 		}
 	}
 	
-	public void colocarEn(int i, int j, Mapeable unElemento) {
+	public void colocarEn(int i, int j, Mapeable unElemento) throws ExcepcionPosicionInvalida {
+		
 		Posicion posicion = new Posicion(i,j);
-		try {
-			this.validarCoordenadas(posicion);
-			mapa.put(posicion, unElemento);
-		}
-		catch (ExcepcionPosicionInvalida e) {
-			
-			System.out.println(e.getMessage());
-		}
+		this.validarCoordenadas(posicion);
+		mapa.put(posicion, unElemento);
 
 	}
 	
 	public boolean validarCoordenadas(Posicion unaPosicion) throws ExcepcionPosicionInvalida {
+		
 		if (!estaDentroDeLimites(unaPosicion)) {
-			throw new ExcepcionPosicionInvalida("Coordendas no validas, fuera de rango");
+			throw new ExcepcionPosicionInvalida("Coordenadas fuera del rango del mapa");
 		}
 		return true;
 	}
 
 	private boolean estaDentroDeLimites(Posicion unaPosicion) {
-		return ((unaPosicion.getX() > 0) && (unaPosicion.getX() < ConstantesAlgoCraft.TAMANIO_MAPA)
-				&& (unaPosicion.getY() > 0) && (unaPosicion.getY() < ConstantesAlgoCraft.TAMANIO_MAPA));
+		
+		return ((unaPosicion.getX() > 0) && (unaPosicion.getX() <= tamanio)
+				&& (unaPosicion.getY() > 0) && (unaPosicion.getY() <= tamanio));
 	}
 	
-	public Mapeable obtenerContenidoEnPosicion(int i, int j){
+	public Mapeable obtenerContenidoEnPosicion(int i, int j) throws ExcepcionPosicionInvalida{
+		
 		Posicion unaPosicion = new Posicion(i,j);
+		this.validarCoordenadas(unaPosicion);
 		return this.mapa.get(unaPosicion);
 	}
 	
-	public void eliminarElementoEnPosicion(Posicion unaPosicion){
+	public void eliminarElementoEnPosicion(int i, int j) throws ExcepcionPosicionInvalida{
+		
+		Posicion unaPosicion = new Posicion(i,j);
+		this.validarCoordenadas(unaPosicion);
 		Pasto pasto = new Pasto();
 		this.mapa.put(unaPosicion,pasto);
 	}
 	
-	public void crearCantidadDeArbolesEnMapa(int cantidadDeArboles){
-		if(cantidadDeArboles < (int)(ConstantesAlgoCraft.TAMANIO_MAPA * 0.1) ) {	//10% del tamaño del mapa
+	public void crearCantidadDeArbolesEnMapa(int cantidadDeArboles) throws ExcepcionPosicionInvalida{
+		
+		if(cantidadDeArboles < (int)(tamanio * 0.1) ) {	  //Como máximo el 10% del tamaño del mapa
 			
 			Random random = new Random();
 			
 			for(int k = 1; k <= cantidadDeArboles; k++) {
 				
 				Arbol unArbol = new Arbol();
-				int i = random.nextInt(ConstantesAlgoCraft.TAMANIO_MAPA)+1;
-				int j = random.nextInt(ConstantesAlgoCraft.TAMANIO_MAPA)+1;
+				int i = random.nextInt(tamanio)+1;  //Genero números randoms para determinar la posicion del Arbol
+				int j = random.nextInt(tamanio)+1;
 				this.colocarEn(i, j, unArbol);
-			}
-			
-		}
-		
-		
+			}	
+		}		
 	}
+	
+	public int distanciaEntreLosPuntos(int x1, int y1, int x2, int y2) {
 		
+		return ((int) Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2)));
+	}
+	
 }
